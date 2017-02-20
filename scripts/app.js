94b5b7ed6de1d20e8a4529e8b879a2f122127f2d
Bill <wkernan@gmail.com>
2017-02-20 13:47:23 -0600

    Angular Material work

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,20 @@
 angular
-    .module('webapp', ['ngMaterial', 'ngMessages', 'ngResource'])
+    .module('webapp', ['ngMaterial', 'ngMessages', 'ngResource', 'ngMdIcons'])
     .controller('DemoCtrl', function($scope, $http, $resource) {
 
+        // API to punk
+        $http.get("https://api.punkapi.com/v2/beers")
+          .success(function(data, status, headers, config) {
+              $scope.beers = data;
+          })
+          .error(function(data, status, headers, config) {
+              console.log("Error " + status);
+          })
+
+        $scope.scrollTop = function() {
+          var content = document.querySelector('md-content');
+          content.scrollTop = 0;
+        }
 
         // use $http or $resource
         // https://api.punkapi.com/v2/beers
